32019da92c8a7919ce9b8351e254bba2e8cd03a9
firmamentholdings <firmamentholdings@gmail.com>
2025-10-23 14:52:25 -0500

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,31 @@
 import Head from 'next/head';
-import React from "react";
+import React, { useEffect } from "react";
 import { MintPage } from "../components";
 
+export default function Home(): JSX.Element {
 
-export default function Home(): JSX.Element{
-  
+  // 🔍 Check environment variables when the page loads
+  useEffect(() => {
+    console.log("🚀 Environment check:");
+    console.log("Candy Machine ID:", process.env.NEXT_PUBLIC_CANDY_MACHINE_ID);
+    console.log("Candy Guard ID:", process.env.NEXT_PUBLIC_CANDY_GUARD_ID);
+    console.log("Network:", process.env.NEXT_PUBLIC_SOLANA_NETWORK);
+    console.log("RPC URL:", process.env.NEXT_PUBLIC_RPC_URL);
+  }, []);
+
   return (
     <>
-			<Head>
-				<title>{process.env.NEXT_PUBLIC_PAGE_TITLE}</title>
-			</Head> 
-			<MintPage 
-				cmId={`${process.env.NEXT_PUBLIC_CANDY_MACHINE_ID}`}
-				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-				//@ts-ignore
-				setNetwork={`${process.env.NEXT_PUBLIC_SOLANA_NETWORK}`}
-				rpcUrl={`${process.env.NEXT_PUBLIC_RPC_URL}`}
-			>
-    	</MintPage>
+      <Head>
+        <title>{process.env.NEXT_PUBLIC_PAGE_TITLE}</title>
+      </Head> 
+      <MintPage 
+        cmId={`${process.env.NEXT_PUBLIC_CANDY_MACHINE_ID}`}
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore
+        setNetwork={`${process.env.NEXT_PUBLIC_SOLANA_NETWORK}`}
+        rpcUrl={`${process.env.NEXT_PUBLIC_RPC_URL}`}
+      >
+      </MintPage>
     </>
   );
 }
